03e5c57d310405325ea03750ed6207c08685388b
Robert Egeland <robegeland@yahoo.com>
2021-12-19 20:51:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(array) {
+    let result = undefined
+    for (let item of array) {
+        if (item.result === 'W') {
+            result = item.year
+            break
+        }
+    }
+    return result
+}
+
+record.find(superbowlWin)
